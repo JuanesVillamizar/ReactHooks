7706168a14a4eba3ef315e2cf17cf23225113764
Juanes <juanes1237@gmail.com>
2021-04-06 19:19:07 -0500

    Se agregan los elementos que se encuentran en la pokedex

--- src/components/ListElementsPokeDex.js
+++ src/components/ListElementsPokeDex.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import PokeDexElement from "./PokeDexElement";
 
 export const ListElementsPokeDex = ({element, data}) => {
     const [names] = useState({
@@ -15,13 +16,15 @@ export const ListElementsPokeDex = ({element, data}) => {
     })
 
     return (
-        <div className='container'>
+        <div className='container p-5 bg-light'>
             <div className="row">
                 <div className="col-12 text-center">
-                    <h3>{names[element.replace('-','_')]} { data.length !== 0 ? 'si hay resultados' : 'no hay resultados' }</h3>
+                    <h3>{names[element.replace('-','_')]} { data.length === 0 && 'no hay resultados' }</h3>
                 </div>
                 <div className="col-12">
-
+                    <div className="row">
+                        {data.map((elementSelected, index) => <PokeDexElement key={index} name={elementSelected.name.toUpperCase()} urlAPI={elementSelected.url} type={names[element.replace('-','_')]}/>)}
+                    </div>
                 </div>
             </div>
         </div>
